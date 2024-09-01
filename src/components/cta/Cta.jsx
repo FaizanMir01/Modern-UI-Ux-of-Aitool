ae8f793de668a55ff0e0198088ad99e22ad3f616
MirFaizan01 <faizanmeer322@gmail.com>
2024-09-01 17:21:07 +0530

    completed the cta section

--- src/components/cta/Cta.jsx
+++ src/components/cta/Cta.jsx
@@ -2,7 +2,15 @@ import './cta.css'
 
 const Cta = () => {
   return (
-    <div>Cta</div>
+    <div className='cta-container section__margin'>
+      <div className="left">
+        <p>Request Early Access to Get Started</p>
+        <h1>Register today & start exploring the endless possiblities.</h1>
+      </div>
+      <div className="right">
+        <button>Get Started</button>
+      </div>
+    </div>
   )
 }
 
