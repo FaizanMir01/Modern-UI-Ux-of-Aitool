15c30eb93cec0da6fe211dae3548451de558b768
MirFaizan01 <faizanmeer322@gmail.com>
2024-09-01 20:34:21 +0530

    completed the footer section

--- src/containers/blog/Blog.jsx
+++ src/containers/blog/Blog.jsx
@@ -6,6 +6,7 @@ import blog03 from '../../assets/blog03.png';
 import blog04 from '../../assets/blog04.png';
 import blog05 from '../../assets/blog05.png';
 
+// eslint-disable-next-line react/prop-types
 const Card = ({ image, title, description }) => {
   return (
     <div className="card">
