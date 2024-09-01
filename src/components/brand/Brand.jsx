30f1d7bcaab2dddc82f07fe6145b01a0ed205de4
MirFaizan01 <faizanmeer322@gmail.com>
2024-09-01 14:09:14 +0530

    completed the whatGpt section

--- src/components/brand/Brand.jsx
+++ src/components/brand/Brand.jsx
@@ -1,8 +1,21 @@
 import './brand.css'
-
+import atlassian from '../../assets/atlassian.png'
+import google from '../../assets/google.png'
+import shopify from '../../assets/shopify.png'
+import slack from '../../assets/slack.png'
+import dropbox from '../../assets/dropbox.png'
 const Brand = () => {
   return (
-    <div>Brand</div>
+    <div className='brand__image-container section__padding'>
+      <div className='image-container'>
+        <img src={atlassian} alt="atlassian logo" />
+        <img src={google} alt="google logo" />
+        <img src={shopify} alt="shopify logo" />
+        <img src={slack} alt="slack logo" />
+        <img src={dropbox} alt="dropbox logo" />
+      </div>
+
+    </div>
   )
 }
 
