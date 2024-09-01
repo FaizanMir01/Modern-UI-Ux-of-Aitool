704bae55a90d4a943bf109584333c02fb15cb9cd
MirFaizan01 <faizanmeer322@gmail.com>
2024-09-01 20:03:38 +0530

    completed the blog section

--- src/containers/blog/Blog.jsx
+++ src/containers/blog/Blog.jsx
@@ -1,5 +1,5 @@
 
-import './Blog.css';
+import './blog.css';
 import blog01 from '../../assets/blog01.png';
 import blog02 from '../../assets/blog02.png';
 import blog03 from '../../assets/blog03.png';
